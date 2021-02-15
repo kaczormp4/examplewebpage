d36fc0b92fca21be7bf58f7a6265cd3ee39d19e0
kaczormp4 <kaczkaoryginal@gmail.com>
2021-02-15 20:13:53 +0100

    slider text

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ function slider(){
     var slider = document.getElementById("slider");
     var sliderButLeft = document.getElementById("sliderButLeft");
     var sliderButRight = document.getElementById("sliderButRight");
+    var sliderH1 = document.getElementById("sliderH1");
+    
+    var info=["","painting walls & wallpapering",'paving & facades & gardens','plasterboard, ceilings & floors']
 
     var min = 0;
     var max = 3;
@@ -9,9 +12,11 @@ function slider(){
         if(min == 0){
             min=max;
             slider.style.backgroundImage =`url("img/s${min}.png")`;
+            sliderH1.innerText = info[min];
             min--;
         }else{
             slider.style.backgroundImage =`url("img/s${min}.png")`;
+            sliderH1.innerText = info[min];
             min--;
         }       
     }
@@ -19,13 +24,16 @@ function slider(){
         if(min == 0){
             min=2;
             slider.style.backgroundImage =`url("img/s${min}.png")`;
+            sliderH1.innerText = info[min];
             min++;
         }else if(min==max){         
             slider.style.backgroundImage =`url("img/s${min}.png")`;
+            sliderH1.innerText = info[min];
             min = 1;
         }
         else{
             slider.style.backgroundImage =`url("img/s${min}.png")`;
+            sliderH1.innerText = info[min];
             min++;
         }
     }
