ce0cbcaf6534fc1af8975f12fda97285441b3b28
kaczormp4 <kaczkaoryginal@gmail.com>
2021-02-20 12:53:50 +0100

    poprawki

--- main.js
+++ main.js
@@ -43,3 +43,51 @@ function slider(){
 }
 
 slider();
+
+// function slider() {
+//     let slider = document.getElementById("slider");
+//     let sliderButLeft = document.getElementById("sliderButLeft");
+//     let sliderButRight = document.getElementById("sliderButRight");
+//     let sliderH1 = document.getElementById("sliderH1");
+
+//     let sliderItems = [
+//         {
+//             img: 's1.png',
+//             content: 'painting walls & wallpapering'
+//         },
+//         {
+//             img: 's2.png',
+//             content: 'paving & facades & gardens'
+//         },
+//         {
+//             img: 's3.png',
+//             content: 'plasterboard, ceilings & floors'
+//         }
+//     ]
+
+//     let min = 0;
+//     let max = sliderItems.length - 1;
+//     const goLeft = () => {
+//         //slider.style.backgroundImage = url(`img/${sliderItems[min].img}`);
+//         console.log(sliderItems[min].img)
+//         sliderH1.innerText = sliderItems[min].content;
+//         if (min == 0) {
+//             min = max;
+//         } else {
+//             min--;
+//         }
+//     }
+//     const goRight = () => {
+//         if (min == sliderItems.length) {
+//             min = 0;
+//         }
+//         slider.style.backgroundImage = url(`img/${sliderItems[min].img}`);
+//         sliderH1.innerText = sliderItems[min].content;
+//         min++;
+//     }
+
+//     sliderButLeft.addEventListener('click', goLeft);
+//     sliderButRight.addEventListener('click', goRight);
+// }
+
+// slider();
